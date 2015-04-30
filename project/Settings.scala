d0a337c13755763934d116847e8c9aa5bc10a0b4
Tom Shull <shull1@illinois.edu>
2015-04-30 11:15:38 -0500

    changed the default scala build version to be 2.11.6
    Note: this is the same as the 2.11 cross compiled version

--- project/Settings.scala
+++ project/Settings.scala
@@ -29,6 +29,7 @@ object Settings {
   val ScalaVersion = "2.11.5"
 
   lazy val basicSettings = Seq(
+    scalaVersion            := "2.11.6",
     crossScalaVersions      := Seq("2.10.5", "2.11.6"),
     resolvers              ++= Dependencies.resolutionRepos,
     fork in run             := true,
